fe9852da7f682c1d3fa4be9f81085cc74ddf3ee7
Stepan Hruda <stepan.hruda@gmail.com>
2015-01-25 13:51:59 -0500

    Fix compiler errors

--- glogbook.go
+++ glogbook.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"code.google.com/p/go-uuid/uuid"
 	"fmt"
 	"github.com/codegangsta/negroni"
 	"github.com/garyburd/redigo/redis"
@@ -23,11 +22,11 @@ func main() {
 	mux.HandleFunc("/door_events", func(w http.ResponseWriter, req *http.Request) {
 		// API ENDPOINT: '/door_events'
 		// PARAMS:
-		// room_uuid: string
+		// room_slug: string
 		// timestamp: rfc3339 timestamp i.e. "2006-01-02T15:04:05Z"
 		// door_state: one of the values from the DoorState const
 
-		roomUuid := req.FormValue("room_uuid")
+		roomSlug := req.FormValue("room_slug")
 		timestamp := req.FormValue("timestamp")
 		time, err := time.Parse(time.RFC3339, timestamp)
 		if err != nil {
@@ -35,7 +34,7 @@ func main() {
 			return
 		}
 		doorState := DoorState(req.FormValue("door_state"))
-		doorEvent := createDoorEvent(roomUuid, time, doorState)
+		doorEvent := createDoorEvent(roomSlug, time, doorState)
 		saveEvent(doorEvent)
 		fmt.Fprintf(w, "door event saved")
 	})
@@ -53,9 +52,9 @@ func saveEvent(doorEvent DoorEvent) (err error) {
 	defer c.Close()
 
 	eventKey := fmt.Sprintf("doorEvent:%s", doorEvent.uuid)
-	roomKey := fmt.Sprintf("room:%s", doorEvent.roomUuid)
-	c.Send("HMSET", eventKey, "roomUuid", doorEvent.roomUuid, "timestamp", doorEvent.timestamp, "state", doorEvent.state)
-	c.Send("HMSET", roomKey, "lastEventTimestamp", doorEvent.timestamp, "lastEventState", doorEvent.state)
+	roomKey := fmt.Sprintf("room:%s", doorEvent.roomSlug)
+	c.Send("HMSET", eventKey, "roomSlug", doorEvent.roomSlug, "timestamp", doorEvent.Timestamp, "state", doorEvent.state)
+	c.Send("HMSET", roomKey, "lastEventTimestamp", doorEvent.Timestamp, "lastEventState", doorEvent.state)
 	c.Flush()
 	c.Receive()
 	c.Receive()
@@ -75,10 +74,10 @@ func loadRooms() (rooms []Room, err error) {
 	for _, key := range roomKeys {
 		parts := strings.Split(key, "room:")
 
-		roomUuid := parts[len(parts)-1]
+		roomSlug := parts[len(parts)-1]
 		roomValues, _ := redis.Strings(c.Do("HGETALL", key))
 		time, _ := time.Parse(time.RFC3339, roomValues[1])
-		room := Room{Uuid: uuid.Parse(roomUuid), LastEventTimestamp: time, Name: roomUuid, LastEventState: DoorState(roomValues[3])}
+		room := Room{Slug: roomSlug, LastEventTimestamp: time, Name: roomSlug, LastEventState: DoorState(roomValues[3])}
 		rooms = append(rooms, room)
 	}
 	return
